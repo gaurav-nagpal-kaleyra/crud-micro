02da34109d7bc2f2c5ea53c4e461590148715111
gaurav0545 <126970363+gaurav0545@users.noreply.github.com>
2023-07-18 22:32:22 +0530

    Update mongo.go

--- config/mongo.go
+++ config/mongo.go
@@ -16,7 +16,7 @@ var Client *mongo.Client
 func MongoDBConnection() error {
 
 	Client, err = mongo.Connect(context.Background(), options.Client().ApplyURI(fmt.Sprintf("mongodb://%v:%v", os.Getenv("MONGO_HOST"), os.Getenv("MONGO_PORT"))))
-	if err := Client.Ping(context.TODO(), readpref.Primary()); err != nil {
+	if err := Client.Ping(context.Background(), readpref.Primary()); err != nil {
 		return err
 	}
 	zap.L().Info("Mongodb connection established")
